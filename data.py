805d59237bb5f7298e8e0b62e0c65af23a458791
Sebastian <jarczewski.sebastian@gmail.com>
2023-11-02 23:23:00 +0100

    Add some comments

--- data.py
+++ data.py
@@ -1,13 +1,19 @@
 import requests
 
 parameters = {
-    "amount": 10,
-    "type": "boolean",
-    "category": 17,
+    "amount": 10,  # Number of questions
+    "type": "boolean",  # Type of questions
+    "category": 17,  # Category number, 17 - Science & Nature
 }
 
-response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+# Making a get request
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)  # Fetch the data from API with parameters
 response.raise_for_status()
+
+# Store the JSON data in a variable "data"
 data = response.json()
 
+# Store a list of questions with their properties
 question_data = data["results"]
+
+# print(question_data)
--- question_model.py
+++ question_model.py
@@ -1,3 +1,6 @@
+# Define the Question class. Objects of such class have two properties: text of a question and the correct answer.
+# When created, two parameters shoud be provided, q_text and q_answer, which originate from
+
 class Question:
 
     def __init__(self, q_text, q_answer):
